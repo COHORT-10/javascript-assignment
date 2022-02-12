17008ed6949be7046ca59006b8f1f085ed1f40a0
Naggayi Daphne Pearl <98743233+Naggayi-Daphne-Pearl@users.noreply.github.com>
2022-02-11 23:33:37 -0800

    Comment on the following operators

---
+++ javascript/operator.js
@@ -0,0 +1,45 @@
+let x = 5;
+let y = 3;
+
+//
+console.log('x + y = ', x + y);  //
+
+//
+console.log('x - y = ', x - y);  //
+
+//
+console.log('x * y = ', x * y);  //
+
+//
+console.log('x / y = ', x / y);  //
+//
+console.log('x % y = ', x % y);   //
+
+// increment-
+console.log('++x = ', ++x); //
+console.log('x++ = ', x++); //
+console.log('x = ', x);     //
+
+//
+console.log('--x = ', --x); //
+console.log('x-- = ', x--); //
+console.log('x = ', x);     //
+
+//
+console.log('x ** y =', x ** y);
+
+//
+console.log(2 == 2); //
+console.log(2 == '2'); //
+
+//
+console.log(3 != 2); //
+console.log('hello' != 'Hello'); //
+
+//
+console.log(2 === 2); //
+console.log(2 === '2'); // false
+
+//
+console.log(2 !== '2'); // true
+console.log(2 !== 2); // false
