25b3f197cf28e18dc7bce8326340c00a2da4784e
liza-A <98731430+liza-A@users.noreply.github.com>
2022-02-13 21:02:14 +0300

    Add files via upload

---
+++ ASAMO.js
@@ -0,0 +1,45 @@
+let x = 5;
+let y = 3;
+
+//addition
+console.log('x + y = ', x + y);  // the result is 8
+
+//subtraction
+console.log('x - y = ', x - y);  //the result is 2
+
+//multiplication
+console.log('x * y = ', x * y);  //the result is 15
+
+//division
+console.log('x / y = ', x / y);  //the result is 1.6666667
+//modulus
+console.log('x % y = ', x % y);   //the result is 2
+
+// increment
+console.log('++x = ', ++x); //the result is 6
+console.log('x++ = ', x++); //the result is 6
+console.log('x = ', x);     //the result is 7
+
+//descrement
+console.log('--x = ', --x); //the result is 6
+console.log('x-- = ', x--); //the result is 6
+console.log('x = ', x);     //the result is 5
+
+//exponentiation operator 
+console.log('x ** y =', x ** y);
+
+//equal to
+console.log(2 == 2); //result is true
+console.log(2 == '2'); //result is true
+
+//not equal to
+console.log(3 != 2); //result is true
+console.log('hello' != 'Hello'); //result is true
+
+// strict equal operator
+console.log(2 === 2); //result is true
+console.log(2 === '2'); // false
+
+//strict inequality operator
+console.log(2 !== '2'); // true
+console.log(2 !== 2); // false
