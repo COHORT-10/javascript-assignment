e46715b34406eb54c73038462a4280c94c7ce76d
HUZAIR SHAFIQ <95339849+uzair510@users.noreply.github.com>
2022-02-13 22:47:55 +0300

    Add files via upload

---
+++ shafiq.js
@@ -0,0 +1,46 @@
+let x = 5;
+let y = 3;
+
+//Addition
+console.log('x + y = ', x + y);  // the result is 8
+
+//Subtraction
+console.log('x - y = ', x - y);  //the result is 2
+
+//Multiplication
+console.log('x * y = ', x * y);  //the result is 15
+
+//Division
+console.log('x / y = ', x / y);  //the result is 1.6666667
+
+//Modulus
+console.log('x % y = ', x % y);   //the result is 2
+
+//Increments (Adds 1 before or after the given value)
+console.log('++x = ', ++x); //the result is 6
+console.log('x++ = ', x++); //the result is 6
+console.log('x = ', x);     //the result is 7
+
+//Decrements
+console.log('--x = ', --x); //the result is 6
+console.log('x-- = ', x--); //the result is 6
+console.log('x = ', x);     //the result is 5
+
+//Exponentiation Operator 
+console.log('x ** y =', x ** y);
+
+//Equal to Operator
+console.log(2 == 2); //result is true
+console.log(2 == '2'); //result is true
+
+//Not equal to Operator
+console.log(3 != 2); //result is true
+console.log('hello' != 'Hello'); //result is true
+
+//Strict equal Operator
+console.log(2 === 2); //result is true
+console.log(2 === '2'); // false
+
+//Strict Inequality Operator
+console.log(2 !== '2'); // true
+console.log(2 !== 2); // false
